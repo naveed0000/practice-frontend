4799b0582c02af3e001c9953e98908fdccf25568
naveed0000 <sknaveed99026@gmail.com>
2025-09-04 01:11:07 +0530

    fixed

--- src/app/custom-component/Global/Sizes.tsx
+++ src/app/custom-component/Global/Sizes.tsx
@@ -90,7 +90,7 @@ export default function Sizes() {
                 key={key}
                 variant="outlined"
                 label={option.title}
-                size="small"
+                size="small"   ///asd
                 {...itemProps}
               />
             );
